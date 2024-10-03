b9b5acab4238ef539861c7bc961c6578dc5d87cf
Stanislav <stanislavstranger@gmail.com>
2024-10-03 13:15:17 +0300

    feat(interfaces): add user interface and enums for role and status
    - Added `UserRole` enum for defining user roles (`admin`, `user`)
    - Added `UserStatus` enum for tracking user status (`online`, `offline`)
    - Created `IUser` interface for user data structure including fields for
    `id`, `username`, `email`, `passwordHash`, `profilePicture`, `role`,
    `status`, `createdAt`, and `updatedAt`

---
+++ libs/interfaces/src/lib/user.interface.ts
@@ -0,0 +1,21 @@
+export enum UserRole {
+  ADMIN = 'admin',
+  USER = 'user',
+}
+
+export enum UserStatus {
+  ONLINE = 'online',
+  OFFLINE = 'offline',
+}
+
+export interface IUser {
+  id: string;
+  username: string;
+  email: string;
+  passwordHash: string;
+  profilePicture?: string;
+  role: UserRole;
+  status: UserStatus;
+  createdAt: Date;
+  updatedAt: Date;
+}
